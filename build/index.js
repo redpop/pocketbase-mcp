16f7040bc6d1128627c39c7034da5e300ae5caed
Breno Yano <brenoyano@gmail.com>
2025-04-09 05:44:19 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { PocketBaseServer } from './server/pocketbase-server.js';
-// Create and run the server instance
-const server = new PocketBaseServer();
-server.run().catch(error => {
-    console.error('Failed to start PocketBase MCP server:', error);
-    process.exit(1);
-});
